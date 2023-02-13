22a0dad15f32ac485a7425e0ffec3527010e7422
Ka Long Ngai <kls8162@gmail.com>
2023-02-13 11:24:06 -0500

    Q3

--- OCaml/learning.ml
+++ OCaml/learning.ml
@@ -9,3 +9,8 @@ let rec last_two list = match list with
   | [] | [_] -> None
   | [x; y] -> Some (x,y)
   | _ :: t -> last_two t;;
+
+(*Q3*)
+let rec atNth list index = match list with
+  | [] -> None
+  | h::t -> if index = 0 then Some (h) else atNth t (index-1);;
