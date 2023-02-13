5de91b0c6a2dd70b97cb399f57bae7b91cc42c26
Ka Long Ngai <kls8162@gmail.com>
2023-02-13 11:13:19 -0500

    Q2

--- OCaml/learning.ml
+++ OCaml/learning.ml
@@ -1,6 +1,11 @@
 (*Q1*)
 let rec last list = match list with
-| [] -> None
-| h::[] -> Some h
-| h::t -> last t;;
+  | [] -> None
+  | h::[] -> Some h
+  | h::t -> last t;;
 
+(*Q2*)
+let rec last_two list = match list with
+  | [] | [_] -> None
+  | [x; y] -> Some (x,y)
+  | _ :: t -> last_two t;;
