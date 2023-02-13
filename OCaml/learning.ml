297320fdc1383553255dc20bf069d10eac502c74
Ka Long Ngai <kls8162@gmail.com>
2023-02-13 11:29:34 -0500

    Q4

--- OCaml/learning.ml
+++ OCaml/learning.ml
@@ -14,3 +14,15 @@ let rec last_two list = match list with
 let rec atNth list index = match list with
   | [] -> None
   | h::t -> if index = 0 then Some (h) else atNth t (index-1);;
+
+(*Q4*)
+(*Non tail-recursive method*)
+let rec length_non_tail list = match list with
+| [] -> 1
+| h::t -> 1 + length_non_tail t;;
+(*Tail-recursive method*)
+let length_tail list = 
+  let rec aux n list = match list with
+    | [] -> n
+    | h::t -> aux (n+1) t
+in aux 0 list;;
