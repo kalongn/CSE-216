2df1b74f87da9b46e361806c5d4a5906b9c2205b
Ka Long Ngai <kls8162@gmail.com>
2023-02-14 13:53:33 -0500

    rec_3 take away

--- OCaml/rec_3.ml
+++ OCaml/rec_3.ml
@@ -14,3 +14,10 @@ minimum_weigh_edge graph;;
 
 (*Q3*)
 List.fold_left (fun( (_,_,x) as min) ((_,_,y) as curr) -> if y < x then curr else min) ("","",max_int) graph;;
+
+(*Higher Order Functions must know *)
+List.fold_left ;;(*List.fold_left (+) 0 list*)
+List.rev ;; (*List.rev list*)
+List.map ;; (*List.map (fun x -> x +1) list*)
+List.filter ;; (*List.filter (fun x -> if x mod 2 =0 then true else false) list*)
+List.iter ;; (*List.iter (+) list*)
