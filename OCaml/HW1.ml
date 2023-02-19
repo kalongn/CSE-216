2de84fbf8653be6affc0cd9a4912b92b269e9e1e
Ka Long Ngai <kls8162@gmail.com>
2023-02-19 13:40:53 -0500

    supposingly working helper functions.

---
+++ OCaml/HW1.ml
@@ -0,0 +1,26 @@
+(*Helper Functions*)
+let reverse_list list =
+  let rec aux acc list = match list with
+    | [] -> acc
+    | h::t -> aux (h::acc) t 
+  in aux [] list
+;;
+
+let rec map_list f list = match list with
+  | [] -> []
+  | h::t -> f(h)::(map_list f t)
+;;
+
+let rec fold_left_list f acc list = match list with
+  | [] -> acc
+  | h::t -> fold_left_list f (f acc h ) t
+;;
+
+let filter_list f list =
+  let rec aux acc list = match list with
+    | [] -> acc
+    | h::t -> if(f(h)) then h::(aux acc t) else (aux acc t)
+  in aux [] list
+;;
+
+(*Q1*)
