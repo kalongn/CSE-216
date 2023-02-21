e8f04370ac621843bfb0894639b5352339f407bf
Ka Long Ngai <kls8162@gmail.com>
2023-02-21 13:35:21 -0500

    Q2

--- OCaml/rec_4.ml
+++ OCaml/rec_4.ml
@@ -2,13 +2,13 @@
 type 'a list = Nil | Cons of 'a * 'a list;;
 let mylist = Cons(1,Cons(2,Nil));;
 let rec length list = match list with
-| Nil -> 0
-| Cons(x,y) -> 1 + length y;;
+  | Nil -> 0
+  | Cons(x,y) -> 1 + length y;;
 
 (*Q1*)
 type ('a,'b) tree =
-| Leaf of 'a
-| Tree of ('a,'b) node
+  | Leaf of 'a
+  | Tree of ('a,'b) node
 and
 ('a,'b) node = {
   operator: 'b;
@@ -18,3 +18,11 @@ and
 
 (*Variant type, or (tree here)*)
 (*Record type, and (node here)*)
+
+(*Q2*)
+let rec nodes_and_leaves tree = match tree with
+  | Leaf x -> ([], [x])
+  | Tree {operator = op; left = x; right = y} -> 
+    let (left_nodes, left_leaves) = nodes_and_leaves x 
+    and (right_nodes, right_leaves) = nodes_and_leaves y
+  in (op :: left_nodes @ right_nodes, left_leaves @ right_leaves);;
