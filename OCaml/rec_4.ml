c3d12056807ce16348b23d8eea2f422d19e26cb9
Ka Long Ngai <kls8162@gmail.com>
2023-02-21 13:33:31 -0500

    Q1

--- OCaml/rec_4.ml
+++ OCaml/rec_4.ml
@@ -4,3 +4,17 @@ let mylist = Cons(1,Cons(2,Nil));;
 let rec length list = match list with
 | Nil -> 0
 | Cons(x,y) -> 1 + length y;;
+
+(*Q1*)
+type ('a,'b) tree =
+| Leaf of 'a
+| Tree of ('a,'b) node
+and
+('a,'b) node = {
+  operator: 'b;
+  left: ('a,'b) tree;
+  right: ('a,'b) tree
+};;
+
+(*Variant type, or (tree here)*)
+(*Record type, and (node here)*)
