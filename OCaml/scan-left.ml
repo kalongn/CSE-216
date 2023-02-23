835b562fd21bd9a164023256d55ee333f217108a
Ka Long Ngai <kls8162@gmail.com>
2023-02-22 21:16:23 -0500

    fixed.

--- OCaml/scan-left.ml
+++ OCaml/scan-left.ml
@@ -8,6 +8,6 @@ let rec last list = match list with
 let scan_left f list = 
   let rec aux f list acc = match list with
   | [] -> acc
-  | h::t -> if(acc = []) then (aux f t (h::acc)) else (aux f t (acc@[(f h (last acc))]))
+  | h::t -> if(acc = []) then (aux f t (h::acc)) else (aux f t (acc@[(f (last acc) h )]))
 in aux f list []
 ;;
