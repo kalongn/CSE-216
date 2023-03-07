66c5c74d75c1dcd564fa9c1376cb751fd798d257
Ka Long Ngai <kls8162@gmail.com>
2023-03-06 20:35:20 -0500

    Q9 Format

--- OCaml/learning.ml
+++ OCaml/learning.ml
@@ -65,8 +65,7 @@ let pack list =
     | [] -> []    (* Can only be reached if original list is empty *)
     | [x] -> (x :: current) :: acc
     | a :: (b :: _ as t) ->
-       if a = b then aux (a :: current) acc t
-       else aux [] ((a :: current) :: acc) t 
+       if a = b then aux (a :: current) acc t else aux [] ((a :: current) :: acc) t 
   in List.rev (aux [] [] list)
 ;;
 
