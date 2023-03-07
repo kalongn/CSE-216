04bc4223a2c9b0ed0cca5e433bdec3017e3b1da2
Ka Long Ngai <kls8162@gmail.com>
2023-03-06 22:28:23 -0500

    Added all helper functions.

--- OCaml/learning.ml
+++ OCaml/learning.ml
@@ -1,3 +1,33 @@
+(*Helper Functions*)
+let reverse_list list =
+  let rec aux acc list = match list with
+    | [] -> acc
+    | h::t -> aux (h::acc) t 
+  in aux [] list
+;;
+
+let rec map_list f list = match list with
+  | [] -> []
+  | h::t -> f(h)::(map_list f t)
+;;
+
+let rec fold_left_list f acc list = match list with
+  | [] -> acc
+  | h::t -> fold_left_list f (f acc h ) t
+;;
+
+let filter_list f list =
+  let rec aux acc list = match list with
+    | [] -> acc
+    | h::t -> if(f(h)) then h::(aux acc t) else (aux acc t)
+  in aux [] list
+;;
+
+let rec exist_list f list = match list with
+  | [] -> false
+  | h::t -> if(f(h)) then true else exist_list f t
+;;
+
 (*Q1*)
 let rec last list = match list with
   | [] -> None
