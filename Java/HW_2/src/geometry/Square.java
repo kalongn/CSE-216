841bc1991fd12b9ceef9658fea8f9f6a350a9b7c
Ka Long Ngai <kls8162@gmail.com>
2023-03-28 12:00:29 -0400

    Updated: Square class implementation.

--- Java/HW_2/src/geometry/Square.java
+++ Java/HW_2/src/geometry/Square.java
@@ -2,29 +2,128 @@ package geometry;
 
 public class Square extends Shape {
 
+    public final Point a, b, c, d;
+
     @Override
     public Point center() {
-        return null; // TODO: part of the assignment
+        return new Point("center", round((a.x + c.x) / 2.0), round((a.y + c.y) / 2.0));
     }
 
     @Override
     public Square rotateBy(int degrees) {
-        return null; // TODO: part of the assignment
+        Point[] points = new Point[] { a, b, c, d };
+        for (int i = 0; i < points.length; i++) {
+            points[i] = rotatePoint(points[i], degrees);
+        }
+        return new Square(points[0], points[1], points[2], points[3]);
     }
 
     @Override
     public Shape translateBy(double x, double y) {
-        return null; // TODO: part of the assignment
+        Point[] points = new Point[] { a, b, c, d };
+        for (int i = 0; i < points.length; i++) {
+            points[i] = translatePoint(points[i], x, y);
+        }
+        return new Square(points[0], points[1], points[2], points[3]);
     }
 
     @Override
     public String toString() {
-        return null; // TODO: part of the assignment
+        Point[] points = new Point[] { a, b, c, d };
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < points.length; i++) {
+            Point rounded = new Point(points[i].name, round(points[i].x), round(points[i].y));
+            sb.append(rounded.toString());
+            if (i != points.length - 1) {
+                sb.append(",");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
     public Square(Point a, Point b, Point c, Point d) {
-        // TODO: part of the assignment
+        if (!isValidSquare(a, b, c, d)) {
+            throw new IllegalArgumentException("Input points cannot form a valid square.");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
     }
 
+    private static double edgesLength(Point p1, Point p2) {
+        if (p1.x == p2.x && p1.y == p2.y) {
+            return 0.0;
+        }
+        return round(Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow(p1.y - p2.y, 2)));
+    }
 
+    private boolean isValidSquare(Point a, Point b, Point c, Point d) {
+        if (edgesLength(a, b) == edgesLength(b, c)
+                && edgesLength(c, d) == edgesLength(d, a)
+                && edgesLength(b, c) == edgesLength(c, d)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static Point rotatePoint(Point p, int degrees) {
+        double newX = p.x * Math.cos(Math.toRadians(degrees)) - p.y * Math.sin(Math.toRadians(degrees));
+        double newY = p.x * Math.sin(Math.toRadians(degrees)) + p.y * Math.cos(Math.toRadians(degrees));
+        double lowerBoundTolerance = 1e-16, upperBoundTolerance = 0.9999999999999998;
+        if (lowerBoundTolerance > Math.abs(newX)) {
+            newX = 0.0;
+        }
+        if (upperBoundTolerance < Math.abs(newX)) {
+            newX = round(newX);
+        }
+        if (lowerBoundTolerance > Math.abs(newY)) {
+            newY = 0.0;
+        }
+        if (upperBoundTolerance < Math.abs(newY)) {
+            newY = round(newY);
+        }
+        return new Point(p.name, newX, newY);
+    }
+
+    private static Point translatePoint(Point p, double x, double y) {
+        return new Point(p.name, p.x + x, p.y + y);
+    }
+
+    private static double round(double value) {
+        value *= 100;
+        value = Math.round(value);
+        value /= 100;
+        return value;
+    }
+
+    public static void main(String... args) {
+        // Normal square:
+        Shape sq1 = new Square(new Point("upper-right", 1, 1), new Point("upper-left", 0, 1),
+                new Point("lower-left", 0, 0), new Point("lower-right", 1, 0));
+        // Fail case square:
+        try {
+            Shape sq2 = new Square(new Point("lower-right", 1, 0), new Point("upper-left", 0, 1),
+                    new Point("lower-left", 0, 0), new Point("upper-right", 1, 1));
+            System.out.println(sq2);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Exception test pass");
+        }
+        // printing square:
+        System.out.println(sq1);
+        // Rotate Square:
+        sq1 = ((Square) sq1).rotateBy(90);
+        System.out.println(sq1);
+        sq1 = ((Square) sq1).rotateBy(390);
+        System.out.println(sq1);
+        sq1 = ((Square) sq1).rotateBy(330);
+        System.out.println(sq1);
+        sq1 = ((Square) sq1).translateBy(100, -100);
+        System.out.println(sq1);
+
+        // center of Square:
+        System.out.println(sq1.center());
+    }
 }
