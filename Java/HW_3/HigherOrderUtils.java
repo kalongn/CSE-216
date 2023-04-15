e43c40759129cafada8e0999e50796d87d8ed46f
Ka Long Ngai <kls8162@gmail.com>
2023-04-14 21:56:12 -0400

    Updated: HW3 Q3 done (untested).

--- Java/HW_3/HigherOrderUtils.java
+++ Java/HW_3/HigherOrderUtils.java
@@ -1,3 +1,66 @@
-public class HigherOrderUtils {
+import java.util.function.BiFunction;
 
+public class HigherOrderUtils {
+    public static interface NamedBiFunction<T, U, R> extends BiFunction<T, U, R> {
+        String name();
+    }
+
+    public static final NamedBiFunction<Double, Double, Double> add = new NamedBiFunction<>() {
+
+        @Override
+        public Double apply(Double t, Double u) {
+            return t + u;
+        }
+
+        @Override
+        public String name() {
+            return "plus";
+        }
+
+    };
+
+    public static final NamedBiFunction<Double, Double, Double> subtract = new NamedBiFunction<>() {
+
+        @Override
+        public Double apply(Double t, Double u) {
+            return t - u;
+        }
+
+        @Override
+        public String name() {
+            return "minus";
+        }
+
+    };
+
+    public static final NamedBiFunction<Double, Double, Double> multiply = new NamedBiFunction<>() {
+
+        @Override
+        public Double apply(Double t, Double u) {
+            return t * u;
+        }
+
+        @Override
+        public String name() {
+            return "mult";
+        }
+
+    };
+
+    public static final NamedBiFunction<Double, Double, Double> div = new NamedBiFunction<>() {
+
+        @Override
+        public Double apply(Double t, Double u) {
+            if (u == 0) {
+                throw new java.lang.ArithmeticException("Cannot divide by 0.");
+            }
+            return t / u;
+        }
+
+        @Override
+        public String name() {
+            return "div";
+        }
+
+    };
 }
