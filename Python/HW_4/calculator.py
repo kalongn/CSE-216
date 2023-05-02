7cf56a3b62010daf2fe131f8fe83feaa5bc7f11a
Ka Long Ngai <kls8162@gmail.com>
2023-05-02 11:38:18 -0400

    Modified: Division by 0 edge cases.

--- Python/HW_4/calculator.py
+++ Python/HW_4/calculator.py
@@ -42,11 +42,13 @@ def calculate() -> float:
     first_number = get_number("Enter the first number:")
     arithmetic_operator = get_operator("Enter the operator:")
     second_number = get_number("Enter the second number:")
+    if (arithmetic_operator == "/" and second_number == 0.0):
+        raise ValueError("Cannot divide by 0.")
     return eval(f'{first_number} {arithmetic_operator} {second_number}')
 
 
 class Calculator:
-    def __init__(self, first_numer_prompt: str, arithmetic_operator_prompt: str, second_number_prompt: str, goodbye_messg=None, halt_prompt="Would you like to continue?"):
+    def __init__(self, first_numer_prompt: str, arithmetic_operator_prompt: str, second_number_prompt: str, goodbye_messg=None, halt_prompt: str = "Would you like to continue?") -> None:
         self.__first_numer_prompt = first_numer_prompt
         self.__arithmetic_operator_prompt = arithmetic_operator_prompt
         self.__second_number_prompt = second_number_prompt
@@ -54,23 +56,25 @@ class Calculator:
         self.__halt_prompt = halt_prompt
         self.__calculate_history = []
 
-    def run(self):
+    def run(self) -> None:
         while True:
             first_number = get_number(self.__first_numer_prompt)
             arithmetic_operator = get_operator(
                 self.__arithmetic_operator_prompt)
             second_number = get_number(self.__second_number_prompt)
+            if (arithmetic_operator == "/" and second_number == 0.0):
+                raise ValueError("Cannot divide by 0.")
             result = eval(
                 f'{first_number} {arithmetic_operator} {second_number}')
             self.__calculate_history.append(result)
-            print(str(first_number) + " " +
-                  str(second_number) + " = " + str(result))
+            print(str(first_number) + " " + str(arithmetic_operator) +
+                  " " + str(second_number) + " = " + str(result))
             if (not halt(self.__halt_prompt)):
                 break
         print(
             f"You carried out {len(self.__calculate_history)} calculations. The results were: {'; '.join(str(result) for result in self.__calculate_history)}")
-        if (self.__goodbye_messg is not None):
-            print(self.__goodbye_messg)
+        if (self.__goodbye_messg):
+            print(str(self.__goodbye_messg))
 
 
 if __name__ == "__main__":
