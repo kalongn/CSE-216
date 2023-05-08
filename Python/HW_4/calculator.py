33a66d5c4b7d93782dff0b894acf46d238aac6d3
Ka Long Ngai <kls8162@gmail.com>
2023-05-08 10:12:54 -0400

    Modified: HW_4 calculate method altered.

--- Python/HW_4/calculator.py
+++ Python/HW_4/calculator.py
@@ -1,4 +1,10 @@
 class Calculator:
+
+    __first_number = 0
+    __second_number = 0
+    __operator = ""
+    __result = 0.0
+
     def __init__(self, first_numer_prompt: str, arithmetic_operator_prompt: str, second_number_prompt: str, goodbye_messg: str = "Bye!", halt_prompt: str = "Would you like to continue?") -> None:
         self.__first_numer_prompt = first_numer_prompt
         self.__arithmetic_operator_prompt = arithmetic_operator_prompt
@@ -6,10 +12,6 @@ class Calculator:
         self.__goodbye_messg = goodbye_messg
         self.__halt_prompt = halt_prompt
         self.__calculate_history = []
-        self.__first_number = 0
-        self.__second_number = 0
-        self.__operator = ""
-        self.__result = 0.0
 
     @staticmethod
     def is_float(input_string: str) -> bool:
@@ -57,7 +59,8 @@ class Calculator:
             else:
                 print("Invalid response. Please enter [Y|N].")
 
-    def calculate(self, first_num_prompt: str = "Enter the first number:", arithmetic_operator_prompt: str = "Enter the operator:", second_num_prompt: str = "Enter the second number:") -> float:
+    @classmethod
+    def calculate(cls, first_num_prompt: str = "Enter the first number:", arithmetic_operator_prompt: str = "Enter the operator:", second_num_prompt: str = "Enter the second number:") -> float:
         first_number = Calculator.get_number(first_num_prompt)
         arithmetic_operator = Calculator.get_operator(
             arithmetic_operator_prompt)
@@ -66,15 +69,15 @@ class Calculator:
             raise ZeroDivisionError("Cannot divide by 0.")
         result = float(
             eval(f'{first_number} {arithmetic_operator} {second_number}'))
-        self.__first_number = first_number
-        self.__second_number = second_number
-        self.__operator = arithmetic_operator
-        self.__result = result
+        cls.__first_number = first_number
+        cls.__second_number = second_number
+        cls.__operator = arithmetic_operator
+        cls.__result = result
         return result
 
     def run(self) -> None:
         while True:
-            result = self.calculate(
+            result = Calculator.calculate(
                 self.__first_numer_prompt, self.__arithmetic_operator_prompt, self.__second_number_prompt)
             self.__calculate_history.append(result)
             print(str(self.__first_number) + " " + str(self.__operator) +
